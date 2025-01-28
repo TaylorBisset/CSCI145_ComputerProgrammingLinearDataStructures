7c140b810c9845123cdb9073d84b3fe052cd4e57
Taylor Bisset <taylorbisset@gmail.com>
2025-01-28 10:56:15 -0800

    Added initial for loop

--- CSCI145-P01_DiceRoll/DiceRollem.java
+++ CSCI145-P01_DiceRoll/DiceRollem.java
@@ -1,9 +1,16 @@
 public class DiceRollem {
     public static void main(String[], args) {
+        // Create 5 new dice.
         Dice dice1 = new Dice();
         Dice dice2 = new Dice();
         Dice dice3 = new Dice();
         Dice dice4 = new Dice();
         Dice dice5 = new Dice();
+
+        int allEqual = 0; // Accumulator variable.
+
+        for (i = 0; i < 1000; i++) { // Runs 1000 iterations.
+            
+        }
     }
 }
