63a933dc97d43045ed620253f6cbb6f4dd508c6b
Taylor Bisset <taylorbisset@gmail.com>
2025-01-28 10:48:20 -0800

    Added roll() to Dice class

---
+++ CSCI145-P01_DiceRoll/Dice.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Dice {
+    int diceVal;
+
+    void roll() {
+        Random randomNum = new Random()
+        diceVal = randomNum.nextInt(7) + 1; // Creates a random number between 1 and 7, inclusive.
+    }
+}
