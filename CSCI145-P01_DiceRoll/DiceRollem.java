fee64837a36cb4d0ff21ddf858040ce36c6d8f0e
Taylor Bisset <taylorbisset@gmail.com>
2025-01-28 10:53:16 -0800

    Added five instances of Dice

---
+++ CSCI145-P01_DiceRoll/DiceRollem.java
@@ -0,0 +1,9 @@
+public class DiceRollem {
+    public static void main(String[], args) {
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        Dice dice3 = new Dice();
+        Dice dice4 = new Dice();
+        Dice dice5 = new Dice();
+    }
+}
