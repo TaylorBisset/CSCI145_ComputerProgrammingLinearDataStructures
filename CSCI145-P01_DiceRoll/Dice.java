7bed23a99cd41e77d559b96fbb3e26c5cb591dfc
Taylor Bisset <taylorbisset@gmail.com>
2025-01-28 12:05:57 -0800

    Compiled program

--- CSCI145-P01_DiceRoll/Dice.java
+++ CSCI145-P01_DiceRoll/Dice.java
@@ -4,7 +4,7 @@ public class Dice {
     int diceVal;
 
     void roll() {
-        Random randomNum = new Random()
+        Random randomNum = new Random();
         diceVal = randomNum.nextInt(7) + 1; // Creates a random number between 1 and 7, inclusive.
     }
 }
--- CSCI145-P01_DiceRoll/DiceRollem.java
+++ CSCI145-P01_DiceRoll/DiceRollem.java
@@ -1,5 +1,5 @@
 public class DiceRollem {
-    public static void main(String[], args) {
+    public static void main(String[] args) {
         // Create 5 new dice.
         Dice dice1 = new Dice();
         Dice dice2 = new Dice();
@@ -9,7 +9,7 @@ public class DiceRollem {
 
         int allEqual = 0; // Accumulator variable.
 
-        for (i = 0; i < 1000; i++) { // Runs 1000 iterations.
+        for (int i = 0; i < 1000; i++) { // Runs 1000 iterations.
             dice1.roll();
             dice2.roll();
             dice3.roll();
@@ -20,10 +20,10 @@ public class DiceRollem {
             dice1.diceVal == dice3.diceVal &&
             dice1.diceVal == dice4.diceVal &&
             dice1.diceVal == dice5.diceVal) {
-                System.out.println("Roll number " + i + " all dice show" + dice1.diceVal + ".")
+                System.out.println("Roll number " + i + " all dice show" + dice1.diceVal + ".");
                 allEqual++;
             }
         }
-        System.out.println("A total of " + allEqual + "tosses were the same.")
+        System.out.println("A total of " + allEqual + "tosses were the same.");
     }
 }
