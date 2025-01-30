46c279ef18a12e8afc8c07913c25a4c210edb89a
Taylor Bisset <taylorbisset@gmail.com>
2025-01-29 19:09:15 -0800

    Added ArraysInMethod.java

---
+++ CSCI145-Lab3_Arrays/ArraysInMethods.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class ArraysInMethods {
+    public void run() {
+        int[] data = new int[10];
+        initArray(data, 100);
+        printArray(data);
+    }
+
+    public void initArray(int[] array, int range) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt() % range;
+        }
+    }
+
+    public void printArray(int[] arrayToPrint) {
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.printf("%d ", arrayToPrint[i]);
+        }
+        System.out.println();
+    }
+}
